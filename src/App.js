deefb0effad9fb160c6f7a27f1bbca33a3b1d17f
lara <larataqi12@gmail.com>
2025-08-16 01:58:00 +0300

    edit the style and img of some pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Nav from './Components/Home Page';
+import Nav from './Components/HomePage';
 import style from './Components/Componentstyle.css';
 import { BrowserRouter, createHashRouter } from 'react-router-dom';
-import React, { useState } from "react";
-import Footer from './Components/Footer';
+
 
 
 function App() {
-  const routes=createHashRouter()
   return (
     <>
     <BrowserRouter>
