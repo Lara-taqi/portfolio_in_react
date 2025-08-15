89f1571b2da51d62c02ca9e7f0ecde6ff668d466
Lara-taqi <larataqi12@gmail.com>
2025-08-16 01:37:16 +0300

    Update Testimonials.js

--- src/Components/Testimonials.js
+++ src/Components/Testimonials.js
@@ -13,7 +13,7 @@ function Testimonials() {
       <Card.Img variant="top" src={`${process.env.PUBLIC_URL}/test1.jpg`} alt='testimonials:Sami' />
       <Card.Body>
         <Card.Title className='title'>— Sami Ahmed, Project Manager</Card.Title>
-        <Card.Text>
+        <Card.Text style={{color:'black'}}>
             <FaStar style={{color:'yellow', textAlign:'center',justifyContent:'center',alignContent:'center'}}/> <FaStar style={{color:'yellow'}}/> <FaStar style={{color:'yellow'}} /> <FaStar  style={{color:'yellow'}}/> <FaStar style={{color:'black'}} /><br/>
             <sup style={{color:'black'}}><RiDoubleQuotesL /></sup>
           I worked with Lara on a website development project. They were very professional and provided creative solutions that significantly improved the site's performance and user experience.
@@ -26,7 +26,7 @@ function Testimonials() {
       <Card.Img variant="top" src={`${process.env.PUBLIC_URL}/test2.jpg`} alt='testimonials:Xiao'/>
       <Card.Body>
         <Card.Title className='title'>— xiao mi, Entrepreneur</Card.Title>
-        <Card.Text>
+        <Card.Text style={{color:'black'}}>
         <FaStar style={{color:'yellow'}}/> <FaStar style={{color:'yellow'}}/> <FaStar style={{color:'yellow'}} /> <FaStar  style={{color:'yellow'}}/> <FaStar style={{color:'black'}}/><br/>
        <sup style={{color:'black'}}><RiDoubleQuotesL /></sup>
        Thanks to Lara's technical skills, we successfully launched a fast and efficient web application with a responsive design and excellent user experience. Always punctual and professional.
@@ -40,7 +40,7 @@ function Testimonials() {
       <Card.Img variant="top" src={`${process.env.PUBLIC_URL}/test3.jpg`} alt='testimonials:Mariam' />
       <Card.Body>
         <Card.Title className='title'>— Mariam Khaled, Cybersecurity Officer</Card.Title>
-        <Card.Text>
+        <Card.Text style={{color:'black'}}>
         <FaStar style={{color:'yellow'}}/> <FaStar style={{color:'yellow'}}/> <FaStar style={{color:'yellow'}} /> <FaStar  style={{color:'yellow'}}/> <FaStar style={{color:'black'}}/><br/>
         <sup style={{color:'black'}}><RiDoubleQuotesL /></sup>
        Lara's expertise in penetration testing was evident from the start. They identified critical vulnerabilities and helped us secure our system effectively. Excellent communication and outstanding results.
